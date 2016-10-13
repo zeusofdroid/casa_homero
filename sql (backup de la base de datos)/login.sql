6d8b9fca9bbbcbd83766be3af19c9150119f4bed
Robert <zeusofdroid@gmail.com>
2016-10-13 06:39:34 -0300

    Clases 50 y 51: Trabajamos sobre la base de datos: Creamos tablas, vimos cómo relacionarlas estableciendo enlaces entre las primary keys y las claves foráneas, creamos un procedimiento, vimos cómo hacer consultas complejas (las cuales nos servirán luego para crear nuestro buscador de productos en la página "catálogo.php" sobre la que trabajaremos la próxima clase).
    Actualización: "sql (backup de la base de datos)/login.sql"
    (me guardé dentro del proyecto una copia de la base de datos -aunque por
    cuestiones de seguridad ésto en la práctica no se hace-)

---
+++ sql (backup de la base de datos)/login.sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-10-2016 a las 00:40:13
+-- Versión del servidor: 10.1.10-MariaDB
+-- Versión de PHP: 7.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `login`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `login_usuario` (IN `alias` VARCHAR(80), IN `contra` VARCHAR(32), OUT `existe` INT)  BEGIN
+select count(*) into existe from usuarios where MD5(contra)=password AND (username=alias OR email=alias);
+
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id_categoria` int(11) NOT NULL,
+  `description` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id_categoria`, `description`) VALUES
+(1, 'herramientas mauales'),
+(2, 'herramientas eléctricas'),
+(3, 'elementos de bulonería');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id_prod` int(11) NOT NULL,
+  `description` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio` decimal(6,2) DEFAULT NULL,
+  `id_categoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id_prod`, `description`, `cantidad`, `precio`, `id_categoria`) VALUES
+(1, 'destornillador', 30, '120.00', 1),
+(2, 'agujereadora', 10, '700.00', 2),
+(3, 'tornillos', 400, '1.00', 3),
+(4, 'clavos', 500, '0.00', 3),
+(5, 'arandelas', 250, '1.00', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `username` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `password` char(32) COLLATE utf8_spanish_ci NOT NULL,
+  `email` varchar(80) COLLATE utf8_spanish_ci NOT NULL,
+  `fecha_alta` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `username`, `password`, `email`, `fecha_alta`) VALUES
+(1, 'Claudio', '25d55ad283aa400af464c76d713c07ad', 'cealonso@gmail.com', '2016-09-01 22:31:01'),
+(2, 'María', '263bce650e68ab4e23f28263760b9fa5', 'maria@gmail.com', '2016-09-01 22:31:01'),
+(3, 'Jorge', '5e8667a439c68f5145dd2fcbecf02209', 'jorge@gmail.com', '2016-09-01 22:31:01'),
+(4, 'pepe', '4428c6c474502e61151877825bb41961', 'pepe@gmail.com', '2016-09-01 22:31:01'),
+(5, 'ana', '4428c6c474502e61151877825bb41961', 'ana@gmail.com', '2016-09-01 22:31:01'),
+(6, 'joseluis', 'bd118304a0bfaafe0ab2307a05fe0379', 'joseluis@gmail.com', '0000-00-00 00:00:00'),
+(7, 'beto', '384701c26cd11aea3cb6e9d31c3a2ccb', 'beto@live.com', '2016-09-01 22:52:28');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_prod`),
+  ADD KEY `categorias_productos` (`id_categoria`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_prod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `categorias_productos` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
