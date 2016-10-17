3e2d3eb94487e2afea60be1a449eaec3b166faa3
Robert <zeusofdroid@gmail.com>
2016-10-17 04:42:53 -0300

    Clase 53a Creamos la página "insert_productos.html"
    Actualización:
    -Creación de la página "insert_productos.html"
    -Modificación de "welcome.php" para agregar el hipervínculo a la página
    "insert_productos.html"
    
    [Nota: en la clase 53 vimos dos formas de crear la página
    "insert_productos.html", por eso hago dos commits (53a y 53b).
     En esta versión (53a) creamos una tabla con html usando emmet.
     En la siguiente versión (53b) usamos boootstrap en reemplazo de la
    tabla.]

--- php/welcome.php
+++ php/welcome.php
@@ -1,10 +1,13 @@
 <?php
 session_start();
 if ($_SESSION['logueado']){
-echo "<h1 style='text-align:center'>¡Bienvenido al Sistema!</h1>";
+echo "<h1 style='text-align:center'>¡Bienvenido al sistema!</h1>";
 echo 'Bienvenido '.$_SESSION['username'];
 echo '<br>';
 echo 'Horario de Conexión: '.$_SESSION['time'];
 echo '<br>';
+echo "<a href='logout.php'> Logout </a>";  //Me faltaba agregarlo de una clase anterior, cuando creamos "logout.php"
+echo "<h1 style='text-align:center'> Menu de opciones </h1>";
+echo "<a href='../html/insert_productos.html'> Ingresar nuevos productos </a>";
 }
 ?>
